3c44cbba9386a217b2c34f240de1ad0009172081
Joaquin <maidanajoaquin10@gmail.com>
2023-09-27 21:19:45 -0300

    agregando la api

--- src/app/service/peli-api.service.ts
+++ src/app/service/peli-api.service.ts
@@ -7,4 +7,10 @@ import {HttpClient} from '@angular/common/http';
 export class PeliApiService {
 
   constructor(private http:HttpClient) { }
+
+  baseurl = "https://api.themoviedb.org/3";
+  apikey = "bf43ea876cd3261f8bde8d6c4381c3ed";
+
+  bannerapi = ${this.baseurl}/trending/all/week?api_key=${this.apikey}
+
 }
